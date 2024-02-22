e55dae99dbba57083719536aeea40436e96fa683
MemeWG <memecw1416@gmail.com>
2024-02-22 15:59:12 -0600

    tratamiento formulario

---
+++ src/patient/schemas/tratamiento.schema.ts
@@ -0,0 +1,47 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Tratamiento {
+    @Prop({ required: true })
+    historia_clinica_id: string;
+    
+    @Prop({ required: true })
+    diagnostico_id: string;
+
+    @Prop({ required: false })
+    maestro_id: string;
+
+    @Prop({ required: false })
+    tratamiento: string;
+
+    @Prop({ required: false })
+    Alumno: string;
+
+    @Prop({ required: false })
+    matricula: string;
+
+    @Prop({ required: false })
+    no_expediente: string;
+
+    @Prop({ required: false })
+    fecha_tratamiento: string;
+
+    @Prop({ required: false })
+    observaciones: string;
+
+    @Prop({ required: false })
+    evidencia1: string;
+  
+    @Prop({ required: false })
+    evidencia2: string;
+  
+    @Prop({ required: false })
+    evidencia3: string;
+  
+    @Prop({ required: false })
+    evidencia4: string;
+  
+    @Prop({ required: false })
+    evidencia5: string;
+}
