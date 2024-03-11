8336aafc3d79a01eb91d58b001c0432befa7e36f
carranza12 <franckgta3@gmail.com>
2024-03-11 15:26:01 -0600

    cambios

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,7 @@ async function bootstrap() {
   const app:any = await NestFactory.create(AppModule);
   app.use(json({ limit: '5000mb' }));
   app.use(urlencoded({ extended: true, limit: '5000mb' }));
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
-  });
+  
   app.enableCors({
     origin: 'http://148.212.195.49:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
